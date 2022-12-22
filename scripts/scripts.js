5911f3f03b4f83d8cb2c578d3c58c26e39c644a8
Capricornered <joelleshapiro@gmail.com>
2022-12-22 17:07:32 -0500

    Fixed image file loading in JavaScript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,15 +26,15 @@ window.addEventListener("scroll", logoScroll);
 
 let nav = document.getElementById("menu");
 let hamburger = document.getElementById("hamburger");
-//hamburger.src = "../images/hamburger-01.svg";
+hamburger.src = "images/hamburger-01.svg";
 
 hamburger.onclick = function() {
     if (nav.classList.contains("out")) {
         nav.classList.remove("out");
-        //hamburger.src = "../images/hamburger-01.svg"
+        hamburger.src = "images/hamburger-01.svg"
     } else {
         nav.classList.add("out");
-        //hamburger.src = "../images/x-out.svg"
+        hamburger.src = "images/x-out.svg"
     }
 }
 
@@ -43,7 +43,7 @@ let menuItems = document.getElementsByClassName("menu-item");
 for (let i = 0; i < menuItems.length; i++) {
     menuItems[i].onclick = function() {
         nav.classList.remove("out");
-        hamburger.src = "../images/hamburger-01.svg"
+        hamburger.src = "images/hamburger-01.svg"
     }
 }
 
