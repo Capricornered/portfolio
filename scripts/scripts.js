aad9b4ab4262368ba37048a203c854169462f8ff
Capricornered <joelleshapiro@gmail.com>
2022-12-22 16:57:07 -0500

    Removed issues with hamburger icon

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,15 +26,15 @@ window.addEventListener("scroll", logoScroll);
 
 let nav = document.getElementById("menu");
 let hamburger = document.getElementById("hamburger");
-hamburger.src = "../images/hamburger-01.svg";
+//hamburger.src = "../images/hamburger-01.svg";
 
 hamburger.onclick = function() {
     if (nav.classList.contains("out")) {
         nav.classList.remove("out");
-        hamburger.src = "../images/hamburger-01.svg"
+        //hamburger.src = "../images/hamburger-01.svg"
     } else {
         nav.classList.add("out");
-        hamburger.src = "../images/x-out.svg"
+        //hamburger.src = "../images/x-out.svg"
     }
 }
 
